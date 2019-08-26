09ae3464ebc14fbb7be5f67088b6d916191b8736
raseemGit <raseem.moha@gmail.com>
2019-08-26 10:56:29 +0530

    Day2 Update

--- src/test/java/cucumebr/AddUser.java
+++ src/test/java/cucumebr/AddUser.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -19,12 +20,12 @@ public class AddUser {
 	    driver = new ChromeDriver();
 	    driver.get("http://demo.guru99.com/telecom/");
 	    }
-	@Given("User Click The Add User")
+	@And("User Click The Add User")
 	public void user_Click_The_Add_User() {
 	driver.findElement(By.xpath("(//a[text()='Add Customer'])[1]")).click();  
 	}
 
-	@When("User Should Give The valid Detail")
+	/*@When("User Should Give The valid Detail")
 	public void user_Should_Give_The_valid_Detail() throws InterruptedException {
 		driver.findElement(By.xpath("//label[text()='Done']")).click();  
 		driver.findElement(By.id("fname")).sendKeys("Mohamed");
@@ -33,7 +34,19 @@ public class AddUser {
 		driver.findElement(By.xpath("//textarea[@name='addr']")).sendKeys("Chennai");
 		driver.findElement(By.id("telephoneno")).sendKeys("9876543321");
 		driver.findElement(By.xpath("//input[@name='submit']")).click();  
-		}
+		}*/
+	
+	@When("User Should Give The valid Detail{string},{string},{string},{string},{string}")
+	public void user_Should_Give_The_valid_Detail(String fname, String lname, String email, String address, String phone) {
+		driver.findElement(By.xpath("//label[text()='Done']")).click();  
+		driver.findElement(By.id("fname")).sendKeys(fname);
+		driver.findElement(By.id("lname")).sendKeys(lname);
+		driver.findElement(By.id("email")).sendKeys(email);
+		driver.findElement(By.xpath("//textarea[@name='addr']")).sendKeys(address);
+		driver.findElement(By.id("telephoneno")).sendKeys(phone);
+		driver.findElement(By.xpath("//input[@name='submit']")).click();
+	}
+
 	@Then("I validate the customer id Showing or not")
 	public void i_validate_the_customer_id_Showing_or_not() {
 	Assert.assertTrue(driver.findElement(By.xpath("(//td[@align='center'])[2]")).isDisplayed());
---
+++ src/test/resources/ScenOutline.feature
@@ -0,0 +1,18 @@
+@tag
+Feature: Add A Customer
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario Outline: To Generate A Five Set Of Customer Id
+    Given User Should Launch The Browser
+    And User Click The Add User
+    When User Should Give The valid Detail"<fname>","<lname>","<email>","<address>","<phone>"
+    Then I validate the customer id Showing or not
+
+    Examples: 
+      | fname   | lname  | email                 | address    | phone       |
+      | moha    | raseem | raseem.moha@gmail.com | chennai    |  9442932436 |
+      | krishna | kumar  | krish@gmail.com       | trichy     |  0987654321 |
+      | jai     | shree  | jai@gmail.com         | coimbatore | 09876342321 |
+      | krishna | jai    | shree@gmail.com       | pondy      | 09809874321 |
+      | mohamed | shree  | moha@gmail.com        | coimbatore | 09876542321 |
